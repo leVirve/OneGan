31f88c5d3172bfc743ef5be8610cd3046e91cc73
leVirve <gae.m.project@gmail.com>
2017-11-25 21:00:12 +0800

    update paired transformation for segmentation

--- onegan/io/transform.py
+++ onegan/io/transform.py
@@ -5,31 +5,40 @@
 
 import random
 
-from PIL import Image
+import torch
+import numpy as np
 import torchvision.transforms as F
+from PIL import Image
 
 
 class SegmentationPair():
 
     def __init__(self,
                  target_size=None,
+                 final_transform=False,
                  random_flip=False, random_crop=False):
         self.target_size = target_size
+        self.final_transform = final_transform
         self.random_flip = random_flip
         self.random_crop = random_crop
 
-    def __call__(self, image, segmentation, phase) -> tuple:
+    def __call__(self, image, segmentation, random=False) -> tuple:
         image = image.convert('RGB')
         segment = segmentation.convert('L')
 
-        if phase == 'train':
-            image, segment = self.random_flip(image, segment)
-            image, segment = self.random_crop(image, segment)
+        image, segment = self.random_flip(image, segment)
+        image, segment = self.random_crop(image, segment)
 
         image = F.resize(image, self.target_size, interpolation=Image.BILINEAR)
         segment = F.resize(segment, self.target_size, interpolation=Image.NEAREST)
 
-        return image, segment
+        return self._transform(image, segment)
+
+    def _transform(self, image, segmentation) -> tuple:
+        if self.final_transform:
+            image = F.Normalize(mean=[.5, .5, .5], std=[.5, .5, .5])(F.to_tensor(image))
+            segmentation = torch.from_numpy(np.array(segmentation)).long()
+        return image, segmentation
 
     def random_flip(self, image, segment):
         if self.random_flip and random.random() >= 0.5:
