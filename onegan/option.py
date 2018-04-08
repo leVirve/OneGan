bc350b2669722a0ef16e7e67ec7d61c288e1fe1c
leVirve <gae.m.project@gmail.com>
2018-04-08 16:29:30 +0800

    make option.parser parsed results global

--- onegan/option.py
+++ onegan/option.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2017 Salas Lin (leVirve)
+# Copyright (c) 2017- Salas Lin (leVirve)
 #
 # This software is released under the MIT License.
 # https://opensource.org/licenses/MIT
@@ -16,11 +16,21 @@ class Parser():
         self._add_default_option()
 
     def parse(self, args=None):
-        base_cfg = self._load_option_config(self.config_file)
-        cli_cfg = self.parser.parse_args(args)
-        for k, v in base_cfg.items():
-            if k not in cli_cfg or not getattr(cli_cfg, k):
-                setattr(cli_cfg, k, v)
+        """ Parse the arguments from command-line and config file.
+        The arguments will be overwritten by command-line ones.
+        """
+        global cfg
+
+        file_cfg = self._load_option_config(self.config_file)
+        cli_cfg = self.parser.parse_args(args, namespace=cfg)
+        for key, v in file_cfg.items():
+            if isinstance(v, dict):
+                # support two-level attr-dict
+                attr_dict = AttrDict(v)
+                setattr(cli_cfg, key, attr_dict)
+            if key not in cli_cfg or not getattr(cli_cfg, key):
+                setattr(cli_cfg, key, v)
+
         return cli_cfg
 
     def add_argument(self, *args, **kwargs):
@@ -36,8 +46,30 @@ class Parser():
             return yaml.load(f)
 
 
+class AttrDict(dict):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def __getattr__(self, name):
+        if name in self.__dict__:
+            return self.__dict__[name]
+        elif name in self:
+            return self[name]
+        else:
+            raise AttributeError(name)
+
+    def __setattr__(self, name, value):
+        if name in self.__dict__:
+            self.__dict__[name] = value
+        else:
+            self[name] = value
+
+
 def trainer_option(parser):
     parser.add_argument('--epoch', type=int)
     parser.add_argument('--batch_size', type=int)
     parser.add_argument('--lr', type=float)
     parser.add_argument('--worker', type=int)
+
+cfg = AttrDict()
