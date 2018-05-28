bf68406d0b0721a889459110c71cd2d51baabaa2
leVirve <gae.m.project@gmail.com>
2018-05-28 13:37:03 +0800

    remove cross-entropy2d, use build-in instead

--- onegan/loss.py
+++ onegan/loss.py
@@ -54,16 +54,6 @@ def conditional_input(source, another, conditional):
     return torch.cat((source, another), dim=1) if conditional else another
 
 
-class CrossEntropyLoss2d(nn.Module):
-
-    def __init__(self, weight=None, size_average=True, ignore_index=255):
-        super().__init__()
-        self.nll_loss = nn.NLLLoss(weight, size_average, ignore_index)
-
-    def forward(self, outputs, targets):
-        return self.nll_loss(F.log_softmax(outputs, dim=1), targets)
-
-
 class FocalLoss2d(nn.Module):
 
     def __init__(self, gamma=2, weight=None, size_average=True, ignore_index=255):
