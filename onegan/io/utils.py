577ed10f67d6881a51d3e971d67ea0574239f300
leVirve <gae.m.project@gmail.com>
2018-07-18 15:22:37 +0800

    add async webcam reader

--- onegan/io/utils.py
+++ onegan/io/utils.py
@@ -1,5 +1,11 @@
+import logging
+import threading
+
 import cv2
 import scipy.io as io
+
+
+log = logging.getLogger('onegan.io')
 
 
 def save_mat(name, data):
@@ -37,3 +43,55 @@ class InputStream:
 
     def close(self):
         self.reader.release()
+
+
+class WebcamCaptureAsync:
+
+    def __init__(self, src=0, width=640, height=480):
+        self.src = src
+        self.cap = cv2.VideoCapture(self.src)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
+        self.grabbed, self.frame = self.cap.read()
+        self.started = False
+        self.read_lock = threading.Lock()
+
+        self.num_capture = 0
+        self.num_read = 0
+
+    def start(self):
+        if self.started:
+            log.warn('Asynchronous video capturing has already been started.')
+            return None
+        log.info('Asynchronous video capturing started')
+        self.started = True
+        self.thread = threading.Thread(target=self.update)
+        self.thread.start()
+        return self
+
+    def update(self):
+        while self.started:
+            grabbed, frame = self.cap.read()
+            with self.read_lock:
+                self.grabbed = grabbed
+                self.frame = frame
+                self.num_capture += 1
+
+    def read(self):
+        with self.read_lock:
+            frame = self.frame.copy()
+            grabbed = self.grabbed
+            self.num_read += 1
+        return grabbed, frame
+
+    def stop(self):
+        self.started = False
+        self.thread.join()
+        log.info('Asynchronous video capturing stopped')
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exec_type, exc_value, traceback):
+        self.stop()
+        self.cap.release()
