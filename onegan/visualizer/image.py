8dfeb12547dd763f61566e1d4e37c1b406f1729f
leVirve <gae.m.project@gmail.com>
2018-07-19 16:49:42 +0800

    make visualize.image more general

--- onegan/visualizer/image.py
+++ onegan/visualizer/image.py
@@ -29,12 +29,13 @@ def img_normalize(img, val_range=None):
 def stack_visuals(*args):
     ''' Merge results in one image (R, G, B) naively
     Args:
-        args: each should shape in (N, h, w) or (N, c, h, w)
+        args: each should shape in (N, H, W) or (N, C, H, W)
     '''
     def make_valid_batched_dim(x):
         return x.unsqueeze(1) if x.dim() == 3 else x
 
-    channels = [img_normalize(make_valid_batched_dim(ch)) for ch in args]
+    dtype = args[0].type()
+    channels = [img_normalize(make_valid_batched_dim(ch.type(dtype))) for ch in args]
     padding_channel = torch.zeros_like(channels[0])
 
     IMAGE_CHANNEL = 3
@@ -44,17 +45,28 @@ def stack_visuals(*args):
     return torch.cat(channels, dim=1)
 
 
-def as_rgb_visual(batched_x, vallina=True):
-    # TODO: make it for 3 channels
-    assert batched_x.size(1) <= 3
-    ch1, ch2 = batched_x[:, :1], batched_x[:, 1:]
+def as_rgb_visual(tensor, vallina=False):
+    ''' Make tensor into colorful image
+    Args:
+        tensor: shape in (C, H, W) or (N, C, H, W)
+        vallina: (bool) if True, then use the `stack_visuals`
+    '''
 
-    if vallina:
-        return stack_visuals(ch1, ch2)
-    else:
-        color1, color2 = [.2, .8, .4], [.6, .4, .8]
-        n, _, h, w = batched_x.size()
-        canvas = torch.zeros(n, h, w, 3).to(batched_x)
-        canvas += ch1.squeeze(1).unsqueeze(-1) * torch.tensor([color1]).to(ch1)
-        canvas += ch2.squeeze(1).unsqueeze(-1) * torch.tensor([color2]).to(ch2)
-        return canvas.permute(0, 3, 1, 2)
+    def batched_colorize(batched_x):
+        n, c, h, w = batched_x.size()
+        channels = [batched_x[:, i] for i in range(c)]  # list of N x h x w
+
+        if vallina:
+            return stack_visuals(*channels)
+        else:
+            dtype = channels[0].type()
+            colors = torch.tensor([[.2, .8, .4], [.6, .4, .8], [.8, .4, .2]]).type(dtype)
+
+            canvas = torch.zeros(n, h, w, 3).to(batched_x)
+            for i in range(c):
+                canvas += channels[i].unsqueeze(-1) * colors[i]
+            return canvas.permute(0, 3, 1, 2)
+
+    if tensor.dim() == 3:
+        return batched_colorize(tensor.unsqueeze(0)).squeeze(-1)
+    return batched_colorize(tensor)
